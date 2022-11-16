5ac98b494c46f357eebdbbf7cd85e86203962808
m7kjentz <m.seven.mail@gmail.com>
2022-11-17 00:28:22 +0900

    @Component() デコレーターの説明を追加する

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+// @Component() デコレーター
+// 続くクラスがコンポーネントであることを示す
+// @Component() はセレクター、テンプレート、スタイルなど、コンポーネントに関するメタデータを提供する
+
+// Component() の機能
+// app-product-alerts という selector はコンポーネントを識別する
+// 慣例では、 Angular のコンポーネントセレクターは接頭辞 app- で始まり、その後にコンポーネント名が続く
+// テンプレートとスタイルのファイル名は、コンポーネントの HTML と CSS を参照する
+// @Component() 定義は、コンポーネントの機能を処理するクラス ProductAlertsComponent もエクスポートする
 @Component({
   selector: "app-product-alerts",
   templateUrl: "./product-alerts.component.html",
