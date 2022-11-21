69f83f3a8329427e001989e4e27fea80b9dd9262
m7kjentz <m.seven.mail@gmail.com>
2022-11-22 00:27:42 +0900

    ActivatedRoute を constructor() に注入する

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { Product, products } from "../products";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
-  constructor() { }
+  // ActivatedRouteは、ルーターがロードする各ルーティングコンポーネントごとに固有
+  // ルーティング、そのパラメータ、およびそのルーティングに関連する追加データに関する情報が含まれている
+  // ActivatedRoute を注入することで、サービスを使用するようにコンポーネントを設定している
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
